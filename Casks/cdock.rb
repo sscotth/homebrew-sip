e39bb8bb6127f51af7ff1519420826cf81c584ba
Scott Humphries <sscotth@users.noreply.github.com>
2018-09-18 13:29:47 -0500

    Update cdock to v1.1.3

--- Casks/cdock.rb
+++ Casks/cdock.rb
@@ -1,13 +1,13 @@
 cask 'cdock' do
-  version '0.10.3'
-  sha256 '2e54c96e1a7c61414454171f09c3d0c2c59451bdcb917dcf1cd31f0ce3ee772d'
+  version '1.1.3'
+  sha256 'b324c20ed13afae8e060c2e4ed09a2ffd7617b022ed4da271b4e2a793ad6fa52'
 
-  # github.com/w0lfschild/cDock2 was verified as official when first introduced to the cask
-  url "https://github.com/w0lfschild/cDock2/releases/download/v#{version}/cDock.zip"
-  appcast 'https://github.com/w0lfschild/cDock2/releases.atom',
-          checkpoint: 'dbb05a1906f9b1572f35a4ada2212f784e0d1f0a02f94fa0cdf025d6a004ec02'
-  name 'cDock2'
-  homepage 'https://w0lfschild.github.io/cdock'
+  # raw.githubusercontent.com/w0lfschild/app_updates/master/cDock was verified as official when first introduced to the cask
+  url "https://raw.githubusercontent.com/w0lfschild/app_updates/master/cDock/cDock_#{version}.zip"
+  appcast 'https://raw.githubusercontent.com/w0lfschild/app_updates/master/cDock/appcast.xml',
+          checkpoint: 'ad5283b85f03d59fa232dd7198ea553c12d88bad917c1c60cf5e170c64fe528f'
+  name 'cDock 3'
+  homepage 'https://www.macenhance.com/cdock.html'
 
   depends_on cask: 'easysimbl'
   # depends_on macos: '>= :mavericks'
