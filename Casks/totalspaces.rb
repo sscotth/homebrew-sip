46eb8a4aaf848099c64f6d6148702ede4f2c3e99
Scott Humphries <sscotth@users.noreply.github.com>
2018-09-18 13:44:01 -0500

    Update totalspaces to v2.7.4

--- Casks/totalspaces.rb
+++ Casks/totalspaces.rb
@@ -1,6 +1,6 @@
 cask 'totalspaces' do
-  version '2.6.15'
-  sha256 'e0ef0851ba2922547657051e515e79151205f1d80ecfb730d1be7121839247fb'
+  version '2.7.4'
+  sha256 'a7da90f3ec22370834358200ad783667cc85680c3b85e8d25d0ecc0ff9a1f789'
 
   url "http://downloads.binaryage.com/TotalSpaces#{version.major}-#{version}.dmg"
   appcast "http://updates-s3.binaryage.com/totalspaces#{version.major}.xml",
