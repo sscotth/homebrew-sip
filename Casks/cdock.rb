6f7899de19a8db07e60424c3a993ec99427a3d7e
Scott Humphries <sscotth@users.noreply.github.com>
2019-05-19 12:46:57 -0500

    Update cDock 3 to 1.2.0

--- Casks/cdock.rb
+++ Casks/cdock.rb
@@ -1,11 +1,10 @@
 cask 'cdock' do
-  version '1.1.3'
-  sha256 'b324c20ed13afae8e060c2e4ed09a2ffd7617b022ed4da271b4e2a793ad6fa52'
+  version '1.2.0'
+  sha256 '1efc2a030ca597acad6b25786f12c1fe9917e589afab4bacae4eef61cefce00c'
 
   # raw.githubusercontent.com/w0lfschild/app_updates/master/cDock was verified as official when first introduced to the cask
   url "https://raw.githubusercontent.com/w0lfschild/app_updates/master/cDock/cDock_#{version}.zip"
-  appcast 'https://raw.githubusercontent.com/w0lfschild/app_updates/master/cDock/appcast.xml',
-          checkpoint: 'ad5283b85f03d59fa232dd7198ea553c12d88bad917c1c60cf5e170c64fe528f'
+  appcast 'https://raw.githubusercontent.com/w0lfschild/app_updates/master/cDock/appcast.xml'
   name 'cDock 3'
   homepage 'https://cdock.macenhance.com/'
 
