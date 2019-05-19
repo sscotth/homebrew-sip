d973af89cbb9bbad940cb2b11df8c5f432a3bd4b
Scott Humphries <sscotth@users.noreply.github.com>
2019-05-19 13:09:36 -0500

    Update XtraFinder to `:latest`

--- Casks/xtrafinder.rb
+++ Casks/xtrafinder.rb
@@ -1,10 +1,9 @@
 cask 'xtrafinder' do
-  version '1.0'
-  sha256 'fd1e756490e7302a043bc9a1bef78a79a9c9f483b85b78012eca2d11105c5d0e'
+  version :latest
+  sha256 :no_check
 
   url 'https://www.trankynam.com/xtrafinder/downloads/XtraFinder.dmg'
-  appcast 'https://www.trankynam.com/xtrafinder/releasenotes.html',
-          checkpoint: '22f9971f7dbb9e823c83182cb51b69b59fff5873015ab8ff87c5d0879092bd21'
+  appcast 'https://www.trankynam.com/xtrafinder/releasenotes.html'
   name 'XtraFinder'
   homepage 'https://www.trankynam.com/xtrafinder/'
 
