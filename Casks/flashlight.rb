fda7f0f3e226e69eac63a9395fc61c411f2fb7bc
Scott Humphries <sscotth@users.noreply.github.com>
2019-05-19 13:05:49 -0500

    Update Flashlight to 1.1.0

--- Casks/flashlight.rb
+++ Casks/flashlight.rb
@@ -1,11 +1,10 @@
 cask 'flashlight' do
-  version '1.0.8'
-  sha256 'b324c20ed13afae8e060c2e4ed09a2ffd7617b022ed4da271b4e2a793ad6fa52'
+  version '1.1.0'
+  sha256 'ffda6bf2e1e515ab0c1ec4b70730a9a86c47802d57d88f853fda55c2bbd9f3f1'
 
   # githubusercontent.com/w0lfschild/app_updates/master/Flashlight was verified as official when first introduced to the cask
   url "https://raw.githubusercontent.com/w0lfschild/app_updates/master/Flashlight/Flashlight_#{version}.zip"
-  appcast 'https://raw.githubusercontent.com/w0lfschild/app_updates/master/Flashlight/appcast.xml',
-          checkpoint: 'fdd3c2092553f63601c8bcb6e934cc003573a3d264b31242b4457a41a7cd2edb'
+  appcast 'https://raw.githubusercontent.com/w0lfschild/app_updates/master/Flashlight/appcast.xml'
   name 'Flashlight'
   homepage 'https://github.com/w0lfschild/Flashlight/'
 
