542cd312c7dc1bb3dc8a0c222d9f2b9191c9e95d
Scott Humphries <sscotth@users.noreply.github.com>
2019-05-19 13:42:41 -0500

    Use HTTP for TotalSpaces appcast
    Certificate Error

--- Casks/totalspaces.rb
+++ Casks/totalspaces.rb
@@ -3,7 +3,7 @@ cask 'totalspaces' do
   sha256 '3831c7189471da5fc74fa4a87a9fc3ca110655782e56494e7d6d55095b791ea4'
 
   url "https://downloads.binaryage.com/TotalSpaces#{version.major}-#{version}.dmg"
-  appcast "https://updates-s3.binaryage.com/totalspaces#{version.major}.xml"
+  appcast "http://updates-s3.binaryage.com/totalspaces#{version.major}.xml"
   name 'TotalSpaces'
   homepage 'https://totalspaces.binaryage.com/'
 
