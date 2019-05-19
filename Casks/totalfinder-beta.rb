d23ea81dc25afc0d52c47648606b5babf7b06e14
Scott Humphries <sscotth@users.noreply.github.com>
2019-05-19 13:32:13 -0500

    Use HTTPS for totalfinder-beta

--- Casks/totalfinder-beta.rb
+++ Casks/totalfinder-beta.rb
@@ -2,7 +2,7 @@ cask 'totalfinder-beta' do
   version '1.11.8'
   sha256 'b1f2f80d2bca59d639cb9dabf4973942674b71564bd3c5b8a1029ad2df30b009'
 
-  url "http://downloads.binaryage.com/TotalFinder-#{version}.dmg"
+  url "https://downloads.binaryage.com/TotalFinder-#{version}.dmg"
   name 'TotalFinder'
   homepage 'https://totalfinder.binaryage.com/'
 
