7caaaac963922be2037bd530af94dd4070bd8205
Scott Humphries <sscotth@users.noreply.github.com>
2019-05-19 13:33:31 -0500

    Use HTTPS for TotalSpaces

--- Casks/totalspaces.rb
+++ Casks/totalspaces.rb
@@ -2,8 +2,8 @@ cask 'totalspaces' do
   version '2.7.12'
   sha256 '3831c7189471da5fc74fa4a87a9fc3ca110655782e56494e7d6d55095b791ea4'
 
-  url "http://downloads.binaryage.com/TotalSpaces#{version.major}-#{version}.dmg"
-  appcast "http://updates-s3.binaryage.com/totalspaces#{version.major}.xml"
+  url "https://downloads.binaryage.com/TotalSpaces#{version.major}-#{version}.dmg"
+  appcast "https://updates-s3.binaryage.com/totalspaces#{version.major}.xml"
   name 'TotalSpaces'
   homepage 'https://totalspaces.binaryage.com/'
 
